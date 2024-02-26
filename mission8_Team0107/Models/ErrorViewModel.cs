0f8c5a2398025fe27a493244d0e87ad5410a8468
Ryguy100 <124656432+Ryguy100@users.noreply.github.com>
2024-02-26 10:09:22 -0700

    Add project files.

---
+++ mission8_Team0107/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission8_Team0107.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
