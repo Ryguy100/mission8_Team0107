cc6b38494df0abc75641432f71835a0b7cb2fd33
Ryguy100 <124656432+Ryguy100@users.noreply.github.com>
2024-02-26 18:25:21 -0700

    database setup

---
+++ mission8_Team0107/Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mission8_Team0107.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+
+        [Required(ErrorMessage = "Task is a required field")]
+        public string TaskName { get; set; }
+        public DateTime? DueDate { get; set; }
+        
+        [Required(ErrorMessage = "Quadrant is a required field")]
+        public int? Quadrant { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public bool? IsCompleted { get; set; }
+
+
+    }
+}
